b2c50ba3a3456374ea039feff7d1dc043143c801
Ahmed <a@a.com>
2017-09-26 02:22:43 +0300

    SAVE DATA AND SEND IT TO REMOTE SERVER

--- BatchSendDataToRepo.bat
+++ BatchSendDataToRepo.bat
@@ -4,3 +4,4 @@ git add .
 git commit -m "SAVE DATA AND SEND IT TO REMOTE SERVER"
 git remote add origin https:\\www.github.com\ahmedalrusaini\REMOTESERVERALZAMILCRM02.git
 git push https://github.com/ahmedalrusaini/REMOTESERVERALZAMILCRM02.git
+pause
